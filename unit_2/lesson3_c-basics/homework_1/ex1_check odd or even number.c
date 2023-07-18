74d601696f959aa297da482b922674d6c25af331
mohamed <mohamed@kdk>
2023-07-18 23:57:51 +0300

    add unit_2

---
+++ unit_2/lesson3_c-basics/homework_1/ex1_check odd or even number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+	int n;
+	printf("Enter an integer number you want to check: ");
+	scanf("%d",&n);
+	if(n%2==0)
+		printf("%d number is even",n);
+	else
+		printf("%d is odd",n);
+
+}
